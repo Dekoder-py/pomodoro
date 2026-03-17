022c0fc462a55b70e6bd12cac34e63fbdce1e4b4
Dekoder-py <kyle@codingcorner.dev>
2026-03-17 18:30:34 +1300

    add boilerplate code to make window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+struct PomoApp {}
+
+impl PomoApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for PomoApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Pomodoro!");
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Pomodoro",
+        native_options,
+        Box::new(|cc| Ok(Box::new(PomoApp::new(cc)))),
+    )
 }
