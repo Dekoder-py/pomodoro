9a11d71dfc04b8d76dc4ab43aba6df7c4c67f53c
Dekoder-py <kyle@codingcorner.dev>
2026-03-17 21:30:44 +1300

    start timer stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use std::{thread, time::{Duration, Instant}};
+
 use eframe::egui;
 
-struct PomoApp {}
+struct PomoApp {
+    start_time: Instant,
+    timer_running: bool
+}
 
 impl PomoApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+        Self {
+            start_time: Instant::now(),
+            timer_running: false,
+        }
     }
 }
 
@@ -12,6 +20,22 @@ impl eframe::App for PomoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Pomodoro!");
+
+            ui.add_space(10.0);
+
+            if ui.button("Start").clicked() {
+                self.start_time = Instant::now();
+                self.timer_running = true;
+            }
+
+            if self.timer_running {
+                if self.start_time.elapsed().as_secs() > 300 { // 5 minutes
+                    self.timer_running = false
+                } else {
+                    ui.label(format!("{}", self.start_time.elapsed().as_secs()));
+                    thread::sleep(Duration::from_secs(1));
+                }
+            }
         });
     }
 }
