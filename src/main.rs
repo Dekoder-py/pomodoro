ec231c1f65e56e45a7ffc9cad28cb1eb446b9daf
Dekoder-py <kyle@codingcorner.dev>
2026-03-19 18:04:18 +1300

    fix timer bug, add message for time stopped, at stop button

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{thread, time::{Duration, Instant}};
+use std::time::{Duration, Instant};
 
 use eframe::egui;
 
 struct PomoApp {
     start_time: Instant,
-    timer_running: bool
+    timer_running: bool,
+    has_run: bool,
 }
 
 impl PomoApp {
@@ -12,6 +13,7 @@ impl PomoApp {
         Self {
             start_time: Instant::now(),
             timer_running: false,
+            has_run: false,
         }
     }
 }
@@ -19,22 +21,42 @@ impl PomoApp {
 impl eframe::App for PomoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ctx.request_repaint_after_secs(1.0);
-
             ui.heading("Pomodoro!");
 
             ui.add_space(10.0);
-
-            if ui.button("Start").clicked() {
-                self.start_time = Instant::now();
-                self.timer_running = true;
+            if !self.timer_running {
+                if ui.button("Start").clicked() {
+                    self.start_time = Instant::now();
+                    self.timer_running = true;
+                }
+            } else {
+                if ui.button("Stop").clicked() {
+                    self.has_run = true;
+                    self.timer_running = false;
+                }
             }
 
             if self.timer_running {
-                if self.start_time.elapsed().as_secs() > 300 { // 5 minutes
-                    self.timer_running = false
+                let elapsed = self.start_time.elapsed();
+                let secs = elapsed.as_secs();
+                let remaining = 10 - secs;
+                let remaining_mins = remaining / 60;
+                let remaining_secs = remaining % 60;
+
+                if remaining <= 0 {
+                    // 5 minutes
+                    self.timer_running = false;
+                    self.has_run = true;
                 } else {
-                    ui.label(format!("{}", self.start_time.elapsed().as_secs()));
+                    ui.label(format!("{remaining_mins}:{remaining_secs}"));
+                    let next_sec = Duration::from_secs(secs + 1);
+
+                    ctx.request_repaint_after(next_sec - elapsed);
+                }
+            } else {
+                // timer has finished or timer is not started yet
+                if self.has_run {
+                    ui.label("Great work! Have a break :)");
                 }
             }
         });
